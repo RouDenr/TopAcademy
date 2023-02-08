9ebc40bd0e61c5e841cdefe8ee6fe60283ba4bba
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-02-08 16:43:32 +0000

    tests

--- 08_02/calc.cpp
+++ 08_02/calc.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <iostream>
+
 class Calc {
 
 public:
@@ -19,6 +22,7 @@ public:
 
 class SuperCalc : public Calc {
 
+ public:
     void mult(int a, int b) { // умножение
         //...
     }
@@ -47,5 +51,24 @@ class SaveCalc : public Calc { // сохраняет последнюю опер
     // void minus() {...}
 
     // ~SaveCalc() {}
+
+};
+
+int main() {
+    Calc def_calc;
+    SuperCalc super_calc;
+    SaveCalc save_calc;
+
+    def_calc.minus(100, 50);
+    def_calc.plus(5, 7);
+
+    super_calc.minus(10, 8);
+    super_calc.minus(50, 10);
+    super_calc.mult(7, 11);
+    super_calc.div(512, 16);
+
+    save_calc.minus(77, 8);
+    save_calc.plus(8, 60);
+    std::cout << save_calc.getLastOperation() << "\n";
 
 }
