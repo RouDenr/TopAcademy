e8e8c9c812f23aae49d82981d32cf4bfe013961e
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-02-22 16:55:07 +0000

    work with unq_ptr

--- 22_02/prt_m.cpp
+++ 22_02/prt_m.cpp
@@ -39,14 +39,14 @@ std::map<T *, int> my_ptr<T>::ptrs;
 int foo() {
     // int *ptr = new int();
     // int a = 5;
-    // std::unique_ptr<int> u_ptr(new int());
-    // std::unique_ptr<int> u_ptr2(u_ptr);
+    std::unique_ptr<int *> u_ptr(new int[5]);
+    // std::unique_ptr<int> u_ptr2(u_ptr); // error
 
     // std::shared_ptr<int> sh_ptr(new int());
     // std::shared_ptr<int> sh_ptr2(sh_ptr);
 
-    my_ptr<int> ptr(new int());
-    my_ptr<int> ptr2(ptr);
+    // my_ptr<int> ptr(new int());
+    // my_ptr<int> ptr2(ptr);
 
     std::cout << "Hello\n";
     return 1;
@@ -58,5 +58,18 @@ int main() {
     } catch (const char *err) {
 
     }
+
+    std::unique_ptr<int []> u_ptr(new int[5]);
+    std::unique_ptr<int> u_ptr2(new int(7));
+
+    int tmp = *(u_ptr2.get());
+
+    u_ptr.get()[0] = 1;
+    u_ptr.get()[1] = 2;
+    u_ptr.get()[2] = 3;
+    for (int i = 0; i < 3; i++) {
+        std::cout << u_ptr.get()[i] << " ";
+    }
+    std::cout << "\n";
     // delete [] ptr;
 }
