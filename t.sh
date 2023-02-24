e62202a501b38b596df58297a2b419c40e05af71
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-02-24 04:17:01 +0000

    change data names format

--- t.sh
+++ t.sh
@@ -4,7 +4,7 @@
 separator="_"
 
 # Перебираем каждый файл в текущей директории, имеющий указанную структуру имени
-for file in $(ls -d */ | grep "$separator"); do
+for file in $(ls -d */ | sed 's/.$//' | grep "$separator"); do
 
   # Получаем старый префикс и старое окончание
   old_prefix=$(echo $file | cut -d$separator -f1)
@@ -15,8 +15,9 @@ for file in $(ls -d */ | grep "$separator"); do
   new_suffix=$old_prefix
 
   # Переименовываем файл, используя новое имя
-#   mv $file $new_suffix${separator}*${separator}$new_prefix.extension
+  # mv $file $new_suffix${separator}*${separator}$new_prefix.extension
     echo 1.
-    echo $file $new_prefix${separator}$new_suffix
+    # echo $file $new_prefix${separator}$new_suffix
+    mv $file $new_prefix${separator}$new_suffix
 
 done
